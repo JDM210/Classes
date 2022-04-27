4708a687af9330bec5821616a4e0a8dfb71d86d7
Josh Munday <97485231+JDM210@users.noreply.github.com>
2022-04-27 14:44:21 -0400

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Person
+{
+    public Person()
+    {
+        Name = "Josh";
+    }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+
+    
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+class TestPerson
+{
+    private static void Main()
+    {
+  
+        var person1 = new Person();
+        Console.WriteLine(person1.Name);
+
+        
+        var person2 = new Person("Abbey");
+        Console.WriteLine(person2.Name);
+      
+        Console.WriteLine(person1);
+    }
+}
+
+
